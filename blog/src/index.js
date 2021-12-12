4153e2c01b148d2be34f4176e150ceafca2a8d92
Bhieu <bhieu332001@gmail.com>
2021-12-13 01:46:10 +0700

    create router and controller

--- blog/src/index.js
+++ blog/src/index.js
@@ -2,30 +2,31 @@
 // import { engine } from 'express-handlebars';
 const express = require('express');
 const morgan = require('morgan');
-const { engine }  = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const app = express();
 const path = require('path');
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 //http logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // template engine
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`)
+});
